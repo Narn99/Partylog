5ab513bd9d726eb0a8fc4e9c9a9fb2ae4cf5f2b4
JaeWooJeon <kyt9600@naver.com>
2023-07-14 15:39:23 +0900

    [B-Docs] 오늘 한일 정리

--- RestApi/PartyLog/src/main/java/com/ssafy/partylog/PartyLogApplication.java
+++ RestApi/PartyLog/src/main/java/com/ssafy/partylog/PartyLogApplication.java
@@ -2,9 +2,8 @@ package com.ssafy.partylog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class PartyLogApplication {
 
     public static void main(String[] args) {
