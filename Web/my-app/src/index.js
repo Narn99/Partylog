2c1dcb4be4a71c30a42d890e1b80b73eb4e51e7c
전재우 <kyt9600@naver.com>
2023-07-19 00:16:47 +0900

    [B-FIX] utf-8 인코딩 설정

--- Web/my-app/src/index.js
+++ Web/my-app/src/index.js
@@ -1,15 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import { Provider } from 'react-redux'
-import App from './App'
+import App from "./App";
 // import store from './store'
 
-ReactDOM.createRoot(
-  document.getElementById('root')
-).render(
-  <React.StrictMode>
-    {/* <Provider store={store}> */}
-      <App />
-    {/* </Provider> */}
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  /*
+   * React.StrictMode 적용 시 개발 모드로 인해 useEffect가 2번 실행되어 제거함
+   */
+  <App />
+);
