abcd837f565a027e8fecdf7f3b0d3698ab3877b1
JaeWooJeon <kyt9600@naver.com>
2023-07-20 16:07:01 +0900

    [B-FEAT] 스프링 시큐리티, JWT 발급 클래스 추가

--- RestApi/PartyLog/src/main/java/com/ssafy/partylog/api/controller/UserController.java
+++ RestApi/PartyLog/src/main/java/com/ssafy/partylog/api/controller/UserController.java
@@ -3,6 +3,7 @@ package com.ssafy.partylog.api.controller;
 import com.ssafy.partylog.api.request.UserRequest;
 import com.ssafy.partylog.api.response.UserResponse;
 import com.ssafy.partylog.api.service.UserService;
+import com.ssafy.partylog.token.JwtTokenProvider;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,10 +13,13 @@ import java.util.HashMap;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    private UserService userService;
 
-    private UserController(UserService kakaoLoginService) {
-        this.userService = kakaoLoginService;
+    private UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public UserController(UserService userService, JwtTokenProvider jwtTokenProvider) {
+        this.userService = userService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @PostMapping("/regist")
@@ -35,8 +39,19 @@ public class UserController {
 
     @GetMapping("/login")
     public ResponseEntity<HashMap<String, Object>> loginUser(@RequestParam("code") String code) throws Exception {
-        String access_Token = userService.getAccessToken(code);
-        HashMap<String, Object> userInfo = userService.getUserInfo(access_Token);
-        return new ResponseEntity<HashMap<String, Object>>(userInfo, HttpStatus.OK);
+        HashMap<String, Object> resultMap = new HashMap<>();
+        // 카카오 토큰 발급
+        String kakao_Access_Token = userService.getKakaoAccessToken(code);
+        HashMap<String, Object> userInfo = userService.getKakaoUserInfo(kakao_Access_Token);
+        String kakao_auth_id = userInfo.get("id").toString();
+        if(kakao_auth_id != null || !kakao_auth_id.equals("")) {
+            resultMap.put("access_token", jwtTokenProvider.createToken(kakao_auth_id, "user"));
+            resultMap.put("isSuccess", true);
+        } else {
+            System.out.println("카카오 회원 아님");
+        }
+        // 자체 서비스 토큰 발급
+
+        return new ResponseEntity<HashMap<String, Object>>(resultMap, HttpStatus.OK);
     }
 }
