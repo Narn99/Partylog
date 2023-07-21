2d4c54d9da873f885b1ccbaa18138b5b82beeb17
JaeWooJeon <kyt9600@naver.com>
2023-07-21 09:54:43 +0900

    [B-FIX] UserController 스웨거 수정 및 dependency 추가

--- RestApi/PartyLog/src/main/java/com/ssafy/partylog/api/controller/UserController.java
+++ RestApi/PartyLog/src/main/java/com/ssafy/partylog/api/controller/UserController.java
@@ -3,9 +3,15 @@ package com.ssafy.partylog.api.controller;
 import com.ssafy.partylog.api.request.UserRequest;
 import com.ssafy.partylog.api.response.UserResponse;
 import com.ssafy.partylog.api.service.UserService;
-import io.swagger.v3.oas.annotations.Hidden;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import com.ssafy.partylog.token.JwtTokenProvider;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +21,7 @@ import java.util.HashMap;
 
 @RestController
 @RequestMapping("/user")
-@Tag(name = "User", description = "Swagger 테스트용 API - User")
+@Tag(name = "UserController", description = "사용자 관련 API")
 public class UserController {
 
     private UserService userService;
@@ -27,7 +33,13 @@ public class UserController {
     }
 
     @PostMapping("/regist")
-    @Hidden
+    @Operation(summary = "회원가입", description = "회원가입을 진행합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful Operation", content = @Content(array = @ArraySchema(schema = @Schema(implementation = UserResponse.class)))),
+            @ApiResponse(responseCode = "400", description = "Invalid"),
+            @ApiResponse(responseCode = "404", description = "Not found")
+    })
+    @Parameter(name = "userInfo", description = "회원가입 시 받는 회원정보")
     public ResponseEntity<HashMap<String, Object>> registUser(@RequestBody UserRequest userInfo) throws Exception {
         HashMap<String, Object> resultMap = new HashMap<>();
         String message = "";
@@ -43,12 +55,15 @@ public class UserController {
     }
 
     @GetMapping("/login")
-    @Hidden
+    @Operation(summary = "로그인", description = "로그인을 진행합니다.")
+    @Parameter(name = "code", description = "카카오에서 발급해준 인증코드")
     public ResponseEntity<HashMap<String, Object>> loginUser(@RequestParam("code") String code) throws Exception {
+        System.out.println("카카오 인증 코드: " + code);
         HashMap<String, Object> resultMap = new HashMap<>();
         // 카카오 토큰 발급
         String kakao_Access_Token = userService.getKakaoAccessToken(code);
         HashMap<String, Object> userInfo = userService.getKakaoUserInfo(kakao_Access_Token);
+        System.out.println("카카오 내 정보: " + userInfo);
         String kakao_auth_id = userInfo.get("id").toString();
         if(kakao_auth_id != null || !kakao_auth_id.equals("")) {
             resultMap.put("access_token", jwtTokenProvider.createToken(kakao_auth_id, "user"));
