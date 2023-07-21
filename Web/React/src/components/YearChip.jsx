264a1e273a8c92af30a381b7288affd7c9944bbe
hyunin3 <hjshimel@gmail.com>
2023-07-21 17:44:20 +0900

    [W-Design] 마이페이지 보드 외의 요소들 기능 없이 추가

---
+++ Web/React/src/components/YearChip.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { Chip, Stack, ThemeProvider, createTheme } from '@mui/material';
+
+export default function ClickableChips() {
+  const [selected, setSelected] = useState(null);
+
+  const theme = createTheme({
+    palette: {
+      secondary: {
+        main: '#fbb3c2',
+      },
+    },
+  });
+
+  const handleClick = (label) => {
+    setSelected(label);
+    console.info('You clicked the Chip.');
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Stack direction="row" spacing={1}>
+        <Chip
+          label="전체 연도"
+          variant={selected === "전체 연도" ? "filled" : "outlined"}
+          color={selected === "전체 연도" ? "secondary" : "default"}
+          onClick={() => handleClick("전체 연도")}
+        />
+        <Chip
+          label="2023"
+          variant={selected === "2023" ? "filled" : "outlined"}
+          color={selected === "2023" ? "secondary" : "default"}
+          onClick={() => handleClick("2023")}
+        />
+      </Stack>
+    </ThemeProvider>
+  );
+}
--- Web/React/src/pages/MyPage.jsx
+++ Web/React/src/pages/MyPage.jsx
@@ -4,6 +4,9 @@ import "../components/Timmer"
 import CountdownTimer from "../components/Timmer";
 import SearchFriend from "../components/SearchFriend";
 import molru from "../assets/molru.webp"
+import YearChip from "../components/YearChip";
+// import board from "../assets/board.png"
+
 
 function MyPage(props) {
   const [targetDateTime, setTargetDateTime] = useState("2024-01-01T00:00");
@@ -12,16 +15,18 @@ function MyPage(props) {
     setTargetDateTime(event.target.value);
   };
 
+  
+
   return (
-    <div className="MyPage">
+    <div className="">
       <div className="MyPage-header">
         <h1 className="loginpage-h1">Partylog</h1>
         <SearchFriend />
         <img src={molru} alt="settingimg" className="MyPage-settingimg"/>
       </div>
-      
-        <div className="MyPage-content">
-        
+     
+      <div className="MyPage">
+        <div className="MyPage-profile">
         <img src={molru} alt="profileimg" className="MyPage-profileimg"/>
         <p className="MyPage-nickname">몰루?!</p>
         <p>팔로잉|팔로워</p>
@@ -30,7 +35,13 @@ function MyPage(props) {
       <CountdownTimer targetDateTime={targetDateTime} />
 
       <button className="MyPage-button">라이브로 이동</button>
-      
+      </div>
+       
+      <div className="MyPage-side">
+      <YearChip />
+      <button className="MyPage-button">메시지 작성</button>
+      {/* <img src={board} alt="messageboard" className=""/> */}
+      </div>
       </div>
     </div>
   );
