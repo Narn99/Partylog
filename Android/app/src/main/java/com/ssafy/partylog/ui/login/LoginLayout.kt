bcd01cf79495a210b08153ef6fa597bab00f8c4c
JINWOO CHOI <csw3055@gmail.com>
2023-07-25 15:34:43 +0900

    S09P12A501-73 [A-Feat] 폰트 에셋 추가

--- Android/app/src/main/java/com/ssafy/partylog/ui/login/LoginLayout.kt
+++ Android/app/src/main/java/com/ssafy/partylog/ui/login/LoginLayout.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -12,18 +13,17 @@ import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.ssafy.partylog.R
 
-class LoginLayout {
-    @Composable
-    fun Login() {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Image(painter = painterResource(id = R.drawable.ic_login_logo), contentDescription = "login")
-            Text(text = stringResource(id =R.string.first_greeting))
-            Image(imageVector = ImageVector.vectorResource(id = R.drawable.ic_login_kakaologin), contentDescription = "")
-        }
+@Composable
+fun Login(modifier: Modifier = Modifier.) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+    modifier = modifier) {
+        Image(painter = painterResource(id = R.drawable.ic_login_logo), contentDescription = "login")
+        Text(text = stringResource(id =R.string.first_greeting))
+        Image(imageVector = ImageVector.vectorResource(id = R.drawable.ic_login_kakaologin), contentDescription = "")
     }
-    @Composable
-    @Preview
-    fun Preview() {
-        Login()
-    }
+}
+@Composable
+@Preview
+fun Preview() {
+    Login()
 }
