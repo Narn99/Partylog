e9068c72c8dd4803c42b5f1763f9ef59c9ff9b14
JINWOO CHOI <csw3055@gmail.com>
2023-07-25 15:20:16 +0900

    [A-Feat] 로그인 화면 레이아웃 구성 및 에셋 추가

--- Android/app/src/main/java/com/ssafy/partylog/ui/login/LoginActivity.kt
+++ Android/app/src/main/java/com/ssafy/partylog/ui/login/LoginActivity.kt
@@ -1,4 +1,19 @@
 package com.ssafy.partylog.ui.login
 
-class LoginActivity {
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.ssafy.partylog.ui.theme.PartylogTheme
+
+class LoginActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        setContent {
+            PartylogTheme() {
+            }
+
+        }
+    }
 }
--- Android/app/src/main/java/com/ssafy/partylog/ui/login/LoginLayout.kt
+++ Android/app/src/main/java/com/ssafy/partylog/ui/login/LoginLayout.kt
@@ -1,4 +1,29 @@
 package com.ssafy.partylog.ui.login
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.ssafy.partylog.R
+
 class LoginLayout {
+    @Composable
+    fun Login() {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(painter = painterResource(id = R.drawable.ic_login_logo), contentDescription = "login")
+            Text(text = stringResource(id =R.string.first_greeting))
+            Image(imageVector = ImageVector.vectorResource(id = R.drawable.ic_login_kakaologin), contentDescription = "")
+        }
+    }
+    @Composable
+    @Preview
+    fun Preview() {
+        Login()
+    }
 }
