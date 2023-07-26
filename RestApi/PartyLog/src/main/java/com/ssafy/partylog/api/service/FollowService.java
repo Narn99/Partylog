3eb14324284fa81bfbf3cf6d6a37364c2f046cd6
DYLee <makna114@naver.com>
2023-07-26 10:07:30 +0900

    [B-FEAT] follow í로그인 유저 확인 추가

--- RestApi/PartyLog/src/main/java/com/ssafy/partylog/api/service/FollowService.java
+++ RestApi/PartyLog/src/main/java/com/ssafy/partylog/api/service/FollowService.java
@@ -7,12 +7,12 @@ import java.util.List;
 
 public interface FollowService {
 
-    public void addFollow(int followeeId) throws Exception;
+    public void addFollow(int followNo, int followeeId) throws Exception;
 
-    public void removeFollow(int followeeId) throws Exception;
+    public void removeFollow(int followNo, int followeeId) throws Exception;
 
-    public List<FollowResponse> searchFollowerList(int limit, int offset) throws Exception;
+    public List<FollowResponse> searchFollowerList(int followNo, int limit, int offset) throws Exception;
 
-    public List<FollowResponse> searchFolloweeList(int limit, int offset) throws Exception;
+    public List<FollowResponse> searchFolloweeList(int followNo, int limit, int offset) throws Exception;
 
 }
