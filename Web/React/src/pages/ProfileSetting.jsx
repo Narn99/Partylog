b1cf05b622a67cb83f46108b1d83f337c9452929
hyunin3 <hjshimel@gmail.com>
2023-07-26 09:54:00 +0900

    [W-Design] 닉네임 변경기능 삭제 및 프로필 사진 업로드 버튼 변경

--- Web/React/src/pages/ProfileSetting.jsx
+++ Web/React/src/pages/ProfileSetting.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
+import { Button } from '@mui/material';
+import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
+import { useNavigate } from 'react-router-dom';
 import SearchFriend from "../components/SearchFriend";
 import molru from "../assets/molru.webp";
 
 function ProfileSetting() {
-  const [uploadedImage, setUploadedImage] = useState(null); // 프로필 이미지 상태 변수
-  const [nickname, setNickname] = useState(""); // 닉네임 상태 변수
+  const [uploadedImage, setUploadedImage] = useState(null);
+  const navigate = useNavigate();
 
-  // 이미지 업로드를 처리하는 함수
   const handleImageUpload = (event) => {
     const file = event.target.files[0];
     if (file) {
@@ -18,20 +20,12 @@ function ProfileSetting() {
     }
   };
 
-  // 닉네임 입력을 처리하는 함수
-  const handleNicknameChange = (event) => {
-    setNickname(event.target.value);
-  };
-
-  // 프로필 저장 버튼을 누를 때 실행되는 함수
   const handleProfileSave = () => {
-    if (uploadedImage && nickname) {
-      // 여기에 프로필과 닉네임을 서버에 저장하는 로직을 추가해야함
-      alert("프로필과 닉네임 저장됨!");
-    } else if (!uploadedImage) {
-      alert("프로필 사진을 먼저 업로드하세요.");
+    if (uploadedImage) {
+      alert("프로필 저장됨!");
+      navigate('/MyPage');
     } else {
-      alert("닉네임을 입력하세요.");
+      alert("프로필 사진을 먼저 업로드하세요.");
     }
   };
 
@@ -47,17 +41,21 @@ function ProfileSetting() {
         {uploadedImage ? (
           <img src={uploadedImage} alt="profile-upload" style={{ width: "300px", height: "300px", borderRadius: "50%", margin: "20px" }} />
         ) : (
-          <div style={{ width: "200px", height: "200px", borderRadius: "50%", background: "#f0f0f0", display: "flex", justifyContent: "center", alignItems: "center" }}>
+          <div style={{ width: "300px", height: "300px", borderRadius: "50%", background: "#f0f0f0", display: "flex", justifyContent: "center", alignItems: "center" }}>
             <span style={{ color: "#888" }}>프로필 사진을 업로드하세요</span>
           </div>
         )}
 
-        <input type="file" accept="image/*" onChange={handleImageUpload} style={{ margin: "20px" }} />
+        <Button variant="contained" component="label" style={{ margin: "20px" }}>
+          <PhotoCameraIcon />
+          <input
+            type="file"
+            hidden
+            accept="image/*"
+            onChange={handleImageUpload}
+          />
+        </Button>
 
-        {/* 닉네임 입력란 */}
-        <input type="text" value={nickname} onChange={handleNicknameChange} placeholder="닉네임을 입력하세요" style={{ margin: "10px", padding: "5px", fontSize: "16px", textAlign: "center" }} />
-
-        {/* 프로필 저장 버튼 */}
         <button className="ProfileSetting-button" onClick={handleProfileSave}>프로필 저장</button>
       </div>
     </div>
