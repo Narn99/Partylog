c3d71dafc694a4d42631fd4d4425f306e567f72f
JINWOO CHOI <csw3055@gmail.com>
2023-07-28 16:54:02 +0900

    [A-Hotfix] 로그인 파일 정리

--- Android/app/src/main/java/com/ssafy/partylog/ui/login/LoginActivity.kt
+++ Android/app/src/main/java/com/ssafy/partylog/ui/login/LoginActivity.kt
@@ -1,7 +1,6 @@
 package com.ssafy.partylog.ui.login
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.ssafy.partylog.ui.theme.PartylogTheme
--- Android/app/src/main/java/com/ssafy/partylog/ui/login/UiState.kt
+++
@@ -1,5 +0,0 @@
-package com.ssafy.partylog.ui.login
-
-data class UiState (
-    var test:String = ""
-)
