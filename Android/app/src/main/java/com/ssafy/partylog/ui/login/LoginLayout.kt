8847c811eebeefc322c584ab678c06a7030b03c6
JINWOO CHOI <csw3055@gmail.com>
2023-07-31 10:25:28 +0900

    [A-Design] UI 요소 배치
    Ticket No: #S09P12A501-73

--- Android/app/src/main/java/com/ssafy/partylog/ui/login/LoginLayout.kt
+++ Android/app/src/main/java/com/ssafy/partylog/ui/login/LoginLayout.kt
@@ -1,7 +1,10 @@
 package com.ssafy.partylog.ui.login
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -9,13 +12,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ssafy.partylog.R
 import com.ssafy.partylog.ui.theme.PartylogTheme
 import com.ssafy.partylog.ui.theme.loginTextColor
@@ -23,16 +29,29 @@ import com.ssafy.partylog.ui.theme.maplestory
 
 @Composable
 fun Login(modifier: Modifier = Modifier, font: FontFamily = maplestory) {
-    Surface(modifier = modifier) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = modifier) {
-            Image(painter = painterResource(id = R.drawable.ic_login_logo), contentDescription = "login")
-            Text(text = stringResource(id =R.string.first_greeting),
-                fontFamily = font, fontWeight = FontWeight.Light,
-                color = loginTextColor)
-            Image(imageVector = ImageVector.vectorResource(id = R.drawable.ic_login_kakaologin),
-                contentDescription = "kakaoLogin",
-                modifier = modifier.padding(bottom = 124.dp))
+    Surface(modifier = modifier.fillMaxHeight()) {
+        Column() {
+            Column(horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = modifier.weight(1f).fillMaxWidth(),
+            verticalArrangement = Arrangement.Bottom) {
+                Image(painter = painterResource(id = R.drawable.ic_login_logo), contentDescription = "login",
+                modifier = modifier.padding(bottom = 20.dp).padding(horizontal = 47.dp).fillMaxWidth(),
+                contentScale = ContentScale.FillWidth)
+                Text(text = stringResource(id =R.string.first_greeting),
+                    textAlign = TextAlign.Center,
+                    fontSize = 16.sp,
+                    fontFamily = font, fontWeight = FontWeight.Light,
+                    color = loginTextColor,
+                    modifier = modifier.padding(bottom = 20.dp).fillMaxWidth())
+            }
+
+            Column(modifier = modifier.weight(1f).fillMaxWidth(), verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(imageVector = ImageVector.vectorResource(id = R.drawable.ic_login_kakaologin),
+                    contentDescription = "kakaoLogin",
+                    modifier = modifier.padding(bottom = 124.dp))
+            }
+
         }
     }
 }
