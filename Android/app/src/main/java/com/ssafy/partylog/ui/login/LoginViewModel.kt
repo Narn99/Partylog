2be7e8d99b76f512d87dfe26b461d9f6849355e5
JINWOO CHOI <csw3055@gmail.com>
2023-07-31 16:26:32 +0900

    [A_Fix] 카카오 로직 넣을자리 확보
    Ticket No: #S09P12A501-73

--- Android/app/src/main/java/com/ssafy/partylog/ui/login/LoginViewModel.kt
+++ Android/app/src/main/java/com/ssafy/partylog/ui/login/LoginViewModel.kt
@@ -1,4 +1,16 @@
 package com.ssafy.partylog.ui.login
 
-class LoginViewModel {
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class LoginViewModel: ViewModel() {
+    private val _uiState = MutableStateFlow(LoginState())
+    val uiState: StateFlow<LoginState>
+        get() = _uiState.asStateFlow()
+
+    fun onKakaoSelected() {
+
+    }
 }
