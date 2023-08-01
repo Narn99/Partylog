cb9c6e576d83b5c3de634144e56f8f1af3fff00a
JaeWooJeon <kyt9600@naver.com>
2023-08-01 17:07:29 +0900

    [B-FIX] ãprofile 확인 메소드 추가

--- RestApi/PartyLog/src/main/java/com/ssafy/partylog/api/controller/TestController.java
+++ RestApi/PartyLog/src/main/java/com/ssafy/partylog/api/controller/TestController.java
@@ -2,18 +2,19 @@ package com.ssafy.partylog.api.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/test")
 @Tag(name = "test", description = "Swagger 테스트용 API - test")
 public class TestController {
+
+    @Value("${PROFILE_TYPE}")
+    String profileType;
 
     @GetMapping("/hello")
     @Operation(summary = "summary표기", description = "description표기")
@@ -21,15 +22,8 @@ public class TestController {
         return "hello";
     }
 
-    @GetMapping("/exception")
-    public void exceptionTest(@RequestParam("code") int code) throws Exception {
-        if(code == 1) {
-            throw new IllegalArgumentException();
-        } else if(code == 2) {
-            throw new IOException();
-        } else if(code == 3) {
-            throw new NoSuchElementException();
-        }
-
+    @GetMapping("/profile")
+    public String getProfile() {
+        return profileType;
     }
 }
