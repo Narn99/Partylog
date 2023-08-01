3763151aff5884580786fcfa6260d9f44d2fdd78
JINWOO CHOI <csw3055@gmail.com>
2023-08-01 17:16:40 +0900

    [A_Feat] 카카오 로그인 로직 추가
    Ticket No: #S09P12A501-74

--- Android/app/src/main/java/com/ssafy/partylog/ui/login/LoginViewModel.kt
+++ Android/app/src/main/java/com/ssafy/partylog/ui/login/LoginViewModel.kt
@@ -1,6 +1,9 @@
 package com.ssafy.partylog.ui.login
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.kakao.sdk.user.UserApiClient
+import com.ssafy.partylog.GlobalApplication
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -11,6 +14,24 @@ class LoginViewModel: ViewModel() {
         get() = _uiState.asStateFlow()
 
     fun onKakaoSelected() {
-
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(GlobalApplication.context)) {
+            UserApiClient.instance.loginWithKakaoTalk(GlobalApplication.context) { token, error ->
+                if (error != null) {
+                    Log.e("로그인 실패", error.toString())
+                } else if (token != null) {
+                    Log.d("로그인 성공", "${token.accessToken}")
+                }
+            }
+        }
+        else {
+            UserApiClient.instance.loginWithKakaoAccount(GlobalApplication.context) { token, error ->
+                if (error != null) {
+                    Log.e("로그인 실패", error.toString())
+                }
+                else if (token != null) {
+                    Log.d("로그인 성공", "${token.accessToken}")
+                }
+            }
+        }
     }
 }
