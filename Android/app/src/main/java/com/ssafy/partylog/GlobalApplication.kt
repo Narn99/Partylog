0789e2542c41410a5b601b792b196639db8a5864
JINWOO CHOI <csw3055@gmail.com>
2023-08-01 17:18:08 +0900

    [A_Feat] 카카오sdk 초기화 및 context 변수 생성
    Ticket No: #S09P12A501-74

--- Android/app/src/main/java/com/ssafy/partylog/GlobalApplication.kt
+++ Android/app/src/main/java/com/ssafy/partylog/GlobalApplication.kt
@@ -1,10 +1,23 @@
 package com.ssafy.partylog
 
 import android.app.Application
+import android.content.Context
+import android.util.Log
 import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.util.Utility
 
 class GlobalApplication: Application() {
+    companion object {
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        init()
+        var key = Utility.getKeyHash(this)
+        Log.d("key", key)
+    }
     fun init() {
-        KakaoSdk.init(this, "/y2wTgOzDZUN2l5xLLgGlxY5OKY=")
+        KakaoSdk.init(this, appKey = "a" + "${BuildConfig.NATIVE_APP_KEY}")
     }
 }
