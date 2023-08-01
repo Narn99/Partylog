13783700b8eeebad3c4861b66ec713cd8216af3d
JINWOO CHOI <csw3055@gmail.com>
2023-08-01 17:17:30 +0900

    [A_Feat] GlobalApplication context 초기화 로직 추가
    Ticket No: #S09P12A501-74

--- Android/app/src/main/java/com/ssafy/partylog/ui/login/LoginActivity.kt
+++ Android/app/src/main/java/com/ssafy/partylog/ui/login/LoginActivity.kt
@@ -1,11 +1,12 @@
 package com.ssafy.partylog.ui.login
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.ssafy.partylog.GlobalApplication
 import com.ssafy.partylog.ui.NaviCtrl
 import com.ssafy.partylog.ui.theme.PartylogTheme
-
 class LoginActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,7 +14,7 @@ class LoginActivity: ComponentActivity() {
             PartylogTheme {
                 NaviCtrl()
             }
-
+        GlobalApplication.context = this.baseContext
         }
     }
 }
