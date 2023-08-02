a3cdbe6d473689beee8a87901a54d5c67b189387
JINWOO CHOI <csw3055@gmail.com>
2023-08-02 16:03:26 +0900

    [A_Fix] viewModel scope 변경
    Ticket No: #S09P12A501-74

--- Android/app/src/main/java/com/ssafy/partylog/ui/ScreenState.kt
+++ Android/app/src/main/java/com/ssafy/partylog/ui/ScreenState.kt
@@ -1,12 +1,10 @@
 package com.ssafy.partylog.ui
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.ssafy.partylog.ui.login.Login
-import com.ssafy.partylog.ui.login.LoginViewModel
 import com.ssafy.partylog.ui.main.Main
 
 enum class ScreenState {
@@ -17,15 +15,11 @@ enum class ScreenState {
 
 @Composable
 fun NaviCtrl() {
-    lateinit var loginViewModel: LoginViewModel
     val navController = rememberNavController()
 
     NavHost(navController = navController, startDestination = ScreenState.Login.name) {
         composable(route = ScreenState.Login.name) {
-            Login(onKakaoSelected = {
-                loginViewModel.kakaoLogin()
-            })
-            loginViewModel = LoginViewModel(LocalContext.current)
+            Login()
         }
         composable(route = ScreenState.Main.name) {
 
