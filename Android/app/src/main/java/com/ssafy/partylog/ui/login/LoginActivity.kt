d17c271ae7da463128b0aad8ffc3abe6a17a81fb
JINWOO CHOI <csw3055@gmail.com>
2023-08-02 13:55:40 +0900

    [A_Fix] context 초기화 로직 삭제
    Ticket No: #S09P12A501-74

--- Android/app/src/main/java/com/ssafy/partylog/ui/login/LoginActivity.kt
+++ Android/app/src/main/java/com/ssafy/partylog/ui/login/LoginActivity.kt
@@ -1,10 +1,8 @@
 package com.ssafy.partylog.ui.login
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.ssafy.partylog.GlobalApplication
 import com.ssafy.partylog.ui.NaviCtrl
 import com.ssafy.partylog.ui.theme.PartylogTheme
 class LoginActivity: ComponentActivity() {
@@ -14,7 +12,6 @@ class LoginActivity: ComponentActivity() {
             PartylogTheme {
                 NaviCtrl()
             }
-        GlobalApplication.context = this.baseContext
         }
     }
 }
