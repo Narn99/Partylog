8ece5e605d09d0c853ffcd92a229e52acba6a1ce
JINWOO CHOI <csw3055@gmail.com>
2023-08-04 11:05:43 +0900

    [A_Feat] koin 초기화 로직 추가 및 디버그 키 로깅 삭제
    Ticket No: #S09P12A501-74

--- Android/presentaion/src/main/java/com/ssafy/partylog/GlobalApplication.kt
+++ Android/presentaion/src/main/java/com/ssafy/partylog/GlobalApplication.kt
@@ -1,18 +1,24 @@
 package com.ssafy.partylog
 
 import android.app.Application
-import android.util.Log
 import com.kakao.sdk.common.KakaoSdk
-import com.kakao.sdk.common.util.Utility
+import com.ssafy.partylog.koin.module
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class GlobalApplication: Application() {
     override fun onCreate() {
         super.onCreate()
-        var key = Utility.getKeyHash(this)
-        Log.d("key", key)
         init()
     }
     fun init() {
         KakaoSdk.init(this, appKey = BuildConfig.NATIVE_APP_KEY)
+
+
+
+        startKoin {
+            androidContext(this@GlobalApplication)
+            modules(module)
+        }
     }
 }
