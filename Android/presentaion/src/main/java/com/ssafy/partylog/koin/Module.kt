e84758bc3436bf6461af49bfc60d54b4971cb17d
JINWOO CHOI <csw3055@gmail.com>
2023-08-04 11:04:33 +0900

    [A_Chore] 의존성 주입을 위한 koin 라이브러리 추가
    Ticket No: #S09P12A501-74

---
+++ Android/presentaion/src/main/java/com/ssafy/partylog/koin/Module.kt
@@ -0,0 +1,15 @@
+package com.ssafy.partylog.koin
+
+import com.ssafy.data.repository.LoginRepositoryImpl
+import com.ssafy.domain.usecase.login.CheckKakaoTokenUsecase
+import com.ssafy.partylog.ui.login.stateholder.LoginViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+val module: Module = module {
+
+    single{ CheckKakaoTokenUsecase(LoginRepositoryImpl()) }
+    viewModel { LoginViewModel(get(), get()) }
+
+}
