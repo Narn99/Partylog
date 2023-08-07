055bc3fec690cdfd0f144d47d6593d800dba818c
JINWOO CHOI <csw3055@gmail.com>
2023-08-07 12:39:13 +0900

    [A_Fix] 파라미터 추가
    Ticket No: #S09P12A501-74

--- Android/data/src/main/java/com/ssafy/data/service/login/LoginService.kt
+++ Android/data/src/main/java/com/ssafy/data/service/login/LoginService.kt
@@ -1,13 +1,13 @@
 package com.ssafy.data.service.login
 
+import com.skydoves.sandwich.ApiResponse
 import com.ssafy.data.model.login.KakaoCheckRespDto
-import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface LoginService {
-    @GET("/user/mobile/login")
+    @GET("user/mobile/login/{token}")
     suspend fun kakaoLogin(
         @Path("token") token: String
-    ): KakaoCheckRespDto
+    ): ApiResponse<KakaoCheckRespDto>
 }
