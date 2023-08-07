88635f95ed5a0ff51a07c2b2db5f95ea1aadec5c
JINWOO CHOI <csw3055@gmail.com>
2023-08-07 12:40:13 +0900

    [A_Fix] 카카오 로그인 구현부 삭제, Usecase로 이동
    Ticket No: #S09P12A501-74

--- Android/presentaion/src/main/java/com/ssafy/partylog/ui/login/stateholder/LoginViewModel.kt
+++ Android/presentaion/src/main/java/com/ssafy/partylog/ui/login/stateholder/LoginViewModel.kt
@@ -1,10 +1,9 @@
 package com.ssafy.partylog.ui.login.stateholder
 
 import android.app.Application
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
-import com.kakao.sdk.user.UserApiClient
+import com.orhanobut.logger.Logger
 import com.ssafy.domain.usecase.login.CheckKakaoTokenUsecase
 import com.ssafy.partylog.ui.login.LoginState
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -14,39 +13,22 @@ import kotlinx.coroutines.launch
 
 class LoginViewModel(private val application: Application,
     private val checkKakaoTokenUsecase: CheckKakaoTokenUsecase): AndroidViewModel(
-    application
+        application
 ) {
     private val _uiState = MutableStateFlow(LoginState())
     val uiState: StateFlow<LoginState>
         get() = _uiState.asStateFlow()
 
-    fun kakaoLogin() {
-        if (UserApiClient.instance.isKakaoTalkLoginAvailable(application.applicationContext)) {
-            UserApiClient.instance.loginWithKakaoTalk(application.applicationContext) { token, error ->
-                if (error != null) {
-                    Log.e("로그인 실패", error.toString())
-                } else if (token != null) {
-                    afterKakaoLogin(token.accessToken)
-                    Log.d("로그인 성공", "${token.accessToken}")
-                }
-            }
-        }
-        else {
-            UserApiClient.instance.loginWithKakaoAccount(application.applicationContext) { token, error ->
-                if (error != null) {
-                    Log.e("로그인 실패", error.toString())
-                }
-                else if (token != null) {
-                    afterKakaoLogin(token.accessToken)
-                    Log.d("로그인 성공", "${token.accessToken}")
-                }
-            }
-        }
-    }
 
     fun afterKakaoLogin(token: String) {
+        Logger.d("토큰가지고 서버 호출")
         viewModelScope.launch {
-            checkKakaoTokenUsecase.invoke(token)
+            if (checkKakaoTokenUsecase(token)) {
+
+            }
+            else {
+
+            }
         }
     }
 }
