4d6279b201e305f3fca0a147cd79abace250e005
JINWOO CHOI <csw3055@gmail.com>
2023-08-07 12:37:11 +0900

    [A_Style] 가독성 개선
    Ticket No: #S09P12A501-74

--- Android/presentaion/src/main/java/com/ssafy/partylog/ui/login/LoginLayout.kt
+++ Android/presentaion/src/main/java/com/ssafy/partylog/ui/login/LoginLayout.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
@@ -31,62 +32,79 @@ import com.ssafy.partylog.ui.theme.maplestory
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
-fun Login(modifier: Modifier = Modifier, font: FontFamily = maplestory
+fun Login(
+    modifier: Modifier = Modifier, font: FontFamily = maplestory
 ) {
+
     val viewModel = koinViewModel<LoginViewModel>()
 
-        Image(painter = painterResource(id = R.drawable.bg_login_compose), contentDescription = "bg_login",
-        modifier = modifier.fillMaxSize(), contentScale = ContentScale.Fit)
-        Column() {
-            Column(horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = modifier
-                    .weight(1f)
-                    .fillMaxWidth(),
-            verticalArrangement = Arrangement.Bottom) {
-                TitleFrame(modifier, font)
-            }
-
-            Column(modifier = modifier
+    Image(
+        painter = painterResource(id = R.drawable.bg_login_compose),
+        contentDescription = "bg_login",
+        modifier = modifier.fillMaxSize(),
+        contentScale = ContentScale.Fit
+    )
+    Column() {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier
                 .weight(1f)
-                .fillMaxWidth(), verticalArrangement = Arrangement.Bottom,
-            horizontalAlignment = Alignment.CenterHorizontally) {
-                LoginFrame(modifier, viewModel)
-            }
-
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            TitleFrame(modifier, font)
         }
 
+        Column(
+            modifier = modifier
+                .weight(1f)
+                .fillMaxWidth(), verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            LoginFrame(modifier, viewModel)
+        }
+
+    }
+
 }
+
 @Composable
 fun LoginFrame(modifier: Modifier, viewModel: LoginViewModel) {
     val stateHolder = LoginFrameStateHolder(viewModel)
+    val context = LocalContext.current
     Image(imageVector = ImageVector.vectorResource(id = R.drawable.ic_login_kakaologin),
         contentDescription = "kakaoLogin",
         modifier = modifier
             .padding(bottom = 124.dp)
             .clickable {
-                stateHolder.onKakaoSelected()
+                stateHolder.onKakaoSelected(context)
             })
 
 }
 
 @Composable
 fun TitleFrame(modifier: Modifier = Modifier, font: FontFamily) {
-    Image(painter = painterResource(id = R.drawable.ic_login_logo), contentDescription = "login",
+    Image(
+        painter = painterResource(id = R.drawable.ic_login_logo), contentDescription = "login",
         modifier = modifier
             .padding(bottom = 20.dp)
             .padding(horizontal = 47.dp)
             .fillMaxWidth(),
-        contentScale = ContentScale.FillWidth)
-    Text(text = stringResource(id =R.string.first_greeting),
+        contentScale = ContentScale.FillWidth
+    )
+    Text(
+        text = stringResource(id = R.string.first_greeting),
         textAlign = TextAlign.Center,
         fontSize = 16.sp,
         fontFamily = font, fontWeight = FontWeight.Light,
         color = loginTextColor,
         modifier = modifier
             .padding(bottom = 20.dp)
-            .fillMaxWidth())
+            .fillMaxWidth()
+    )
 
 }
+
 @Composable
 @Preview(showBackground = true)
 fun Preview() {
