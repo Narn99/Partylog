68ebebe555c6f21789932eed9e630dd98e33e6c3
JINWOO CHOI <csw3055@gmail.com>
2023-08-07 12:33:47 +0900

    [A_Fix] invoke 리턴타입 변경
    Ticket No: #S09P12A501-74

--- Android/domain/src/main/java/com/ssafy/domain/usecase/login/CheckKakaoTokenUsecase.kt
+++ Android/domain/src/main/java/com/ssafy/domain/usecase/login/CheckKakaoTokenUsecase.kt
@@ -1,10 +1,9 @@
 package com.ssafy.domain.usecase.login
 
-import com.ssafy.domain.model.login.CheckKakaoToken
 import com.ssafy.domain.repository.LoginRepository
 
 class CheckKakaoTokenUsecase(private val repository: LoginRepository) {
-    suspend fun invoke(token: String): Result<CheckKakaoToken> {
-        return repository.checkKakaoToken(token)
+    suspend operator fun invoke(token: String): Boolean {
+       return repository.checkKakaoToken(token)
     }
 }
