e108926863b368936e96cad31dc31588a058edf5
hyunin3 <hjshimel@gmail.com>
2023-08-07 15:35:27 +0900

    [W-Faet] 기본 이미지가 아니라 유저의 프로필 이미지가 친구 페이지에서 보이도록 수정

--- Web/React/src/pages/MyFriend.jsx
+++ Web/React/src/pages/MyFriend.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import NavBar from "../components/NavBar";
-import molru from "../assets/molru.webp";
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import { styled } from '@mui/system';
 import FollowTabs from '../components/FollowTabs';
+import { useSelector } from "react-redux";
 
 
 const Box1 = styled(Box)(({ theme }) => ({
@@ -28,18 +28,27 @@ const Box1 = styled(Box)(({ theme }) => ({
 
 
 function MyFriend(props) {
- 
+ // useSelector를 사용하여 리듀서에서 정보 받아오는 곳
+  const profileImg = useSelector((state) => {
+    return state.auth.userData.userProfile;
+  });
+
   return (
     <div>
-     
       <NavBar />
-     
-
       <div>
       <Grid container spacing={2}>
         <Grid item sm={12} md={3}>
           <Grid container direction="column" justifyContent="center" alignItems="center">
-            <img src={molru} alt="profileimg" className="UserPage-profileimg" />
+            <img
+              src={profileImg}
+              alt="profileimg"
+              className="UserPage-profileimg"
+              style={{
+                  maxWidth: "280px",
+                  maxHeight: "280px",
+                    }}
+                    />
             <p className="UserPage-nickname">몰?루</p>
           </Grid>
         </Grid>
