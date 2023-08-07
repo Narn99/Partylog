20a011d7e486e27d05c02ed32b89907290233353
JINWOO CHOI <csw3055@gmail.com>
2023-08-07 16:58:40 +0900

    [A_Fix] url 은닉
    Ticket No: #S09P12A501-74

--- Android/data/src/main/java/com/ssafy/data/network/NetworkUtil.kt
+++ Android/data/src/main/java/com/ssafy/data/network/NetworkUtil.kt
@@ -1,29 +1,43 @@
 package com.ssafy.data.network
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.skydoves.sandwich.adapters.ApiResponseCallAdapterFactory
+import com.ssafy.data.BuildConfig
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 
-object NetworkUtil {
-    const val SERVICE_URL = "http://i9a501.p.ssafy.io:8080/partylog/"
-    private val contentType = "application/json".toMediaType()
-    private val logInterceptor = HttpLoggingInterceptor().setLevel(level = HttpLoggingInterceptor.Level.BASIC)
 
-    private val client =
+object NetworkUtil {
+    private const val SERVICE_URL = BuildConfig.BASE_URL
+//    private val logInterceptor = HttpLoggingInterceptor { message -> Logger.t("OKHTTP").i(message) }
+//            Logger.t("OKHTTP").i(it)
+//    }.setLevel(HttpLoggingInterceptor.Level.BODY)
+    private val logInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+
+
+
+    private val client by lazy {
         OkHttpClient.Builder()
 //            .addInterceptor(AuthInterceptor())
             .addInterceptor(logInterceptor)
+            .hostnameVerifier { _, _ -> true }
             .build()
+    }
 
-    val retrofit = Retrofit
-        .Builder()
-        .baseUrl(SERVICE_URL)
-        .addConverterFactory(Json.asConverterFactory(contentType = contentType))
-        .client(client)
-        .build()
+    private val contentType = "application/json".toMediaType()
+    val retrofit: Retrofit by lazy {
+        Retrofit
+            .Builder()
+            .baseUrl(SERVICE_URL)
+            .client(client)
+            .addCallAdapterFactory(ApiResponseCallAdapterFactory.create())
+            .addConverterFactory(Json.asConverterFactory(contentType = contentType))
+            .build()
+    }
 
     inline fun <reified T> create(): T = retrofit.create(T::class.java)
 }
