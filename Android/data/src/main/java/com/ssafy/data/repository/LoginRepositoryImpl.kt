1ca01df57325c8240b401dbf46c689759882bc63
JINWOO CHOI <csw3055@gmail.com>
2023-08-07 12:38:56 +0900

    [A_Fix] 핸들링 로직 변경
    Ticket No: #S09P12A501-74

--- Android/data/src/main/java/com/ssafy/data/repository/LoginRepositoryImpl.kt
+++ Android/data/src/main/java/com/ssafy/data/repository/LoginRepositoryImpl.kt
@@ -1,19 +1,28 @@
 package com.ssafy.data.repository
 
+import com.orhanobut.logger.Logger
+import com.skydoves.sandwich.onError
+import com.skydoves.sandwich.onException
+import com.skydoves.sandwich.onSuccess
 import com.ssafy.data.datasource.remote.LoginDatasource
-import com.ssafy.data.mapper.LoginMapper
-import com.ssafy.domain.model.login.CheckKakaoToken
 import com.ssafy.domain.repository.LoginRepository
 
-class LoginRepositoryImpl: LoginRepository {
-    override suspend fun checkKakaoToken(token: String): Result<CheckKakaoToken> =
-        kotlin.runCatching {
-            LoginMapper.checkKakaoRespToResult(
-                LoginDatasource().checkKakaoToken(token)
-            )
-        }.onSuccess {
+class LoginRepositoryImpl(private val loginDatasource: LoginDatasource): LoginRepository {
+    override suspend fun checkKakaoToken(token: String): Boolean {
 
-        }.onFailure {
-
+        Logger.d("datasource 실행$token")
+        val data = loginDatasource.checkKakaoToken(token)
+        data.onSuccess {
+            true
+//            LoginMapper.checkKakaoRespToResult(this.data)
+            Logger.d("카카오토큰 체크 성공")
+        }.onError {
+            false
+            Logger.d("카카오토큰 체크 실패 err\n$this")
+        }.onException {
+            false
+            Logger.d("카카오토큰 체크 실패 exception\n$this")
         }
+        return false
+    }
 }
