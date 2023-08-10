f174db4286f93f96491fd4f1c2f0d93accd28b59
JaeWooJeon <kyt9600@naver.com>
2023-08-10 13:41:03 +0900

    [B-FIX] 토큰 유효기간 재설정

--- RestApi/PartyLog/src/main/java/com/ssafy/partylog/api/service/UserServiceImpl.java
+++ RestApi/PartyLog/src/main/java/com/ssafy/partylog/api/service/UserServiceImpl.java
@@ -168,11 +168,11 @@ public class UserServiceImpl implements UserService {
     public String createToken(int userNo, String type) throws Exception {
         long tokenValidTime = 0L;
         if(type.equals("access-token")) {
-//            tokenValidTime = 30 * 60 * 1000L; // Access 토큰 유효시간 30분
-            tokenValidTime = 30 * 1000L; // Access 토큰 유효시간 1분
+            tokenValidTime = 30 * 60 * 1000L; // Access 토큰 유효시간 30분
+//            tokenValidTime = 30 * 1000L; // Access 토큰 유효시간 1분
         } else {
-//            tokenValidTime = 14 * 24 * 60 * 60 * 1000L; // Refresh 토큰 유효시간 2주
-            tokenValidTime = 60 * 1000L; // Refresh 토큰 유효시간 5분
+            tokenValidTime = 14 * 24 * 60 * 60 * 1000L; // Refresh 토큰 유효시간 2주
+//            tokenValidTime = 60 * 1000L; // Refresh 토큰 유효시간 5분
         }
         return JwtUtil.createJwt(userNo, type, JWT_SECRET_KEY, tokenValidTime);
     }
