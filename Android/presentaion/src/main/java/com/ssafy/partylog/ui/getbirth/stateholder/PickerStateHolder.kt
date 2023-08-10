65b49a07af75d0a9311b41bdbfdd10e8c0eb80f6
JINWOO CHOI <csw3055@gmail.com>
2023-08-10 13:41:57 +0900

    [A_Fix] 상태변수 추가
    Ticket No: #S09P12A501-111

--- Android/presentaion/src/main/java/com/ssafy/partylog/ui/getbirth/stateholder/PickerStateHolder.kt
+++ Android/presentaion/src/main/java/com/ssafy/partylog/ui/getbirth/stateholder/PickerStateHolder.kt
@@ -1,5 +1,8 @@
 package com.ssafy.partylog.ui.getbirth.stateholder
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -9,16 +12,17 @@ class PickerStateHolder {
     private val _uiState = MutableStateFlow(PickerState())
     var uiState: StateFlow<PickerState> = _uiState.asStateFlow()
 
+    var year by mutableStateOf(setDefaultYear())
+    var month by mutableStateOf(6)
+    var day by mutableStateOf(15)
+
     private val days = arrayOf(31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
 
-    fun setVal() {
-        Calendar.getInstance().get(Calendar.YEAR)
-    }
 
     fun setDefaultYear(): Int = Calendar.getInstance().get(Calendar.YEAR) - 20
     fun getMaxYear(): Int = Calendar.getInstance().get(Calendar.YEAR)
     fun getMinYear(): Int = Calendar.getInstance().get(Calendar.YEAR) - 150
-    fun getMaxday(yy:Int = uiState.value.year, mm: Int = uiState.value.month): Int {
+    fun getMaxday(yy:Int = year, mm: Int = month): Int {
         return if (mm != 2) days[mm - 1]
         else {
             if (yy%4 == 0 && yy%100 == 0 && yy%400 == 0) 28
@@ -28,9 +32,6 @@ class PickerStateHolder {
         }
     }
 
-    fun setYear(yy: Int) = _uiState.value.year
-    fun setMonth(mm: Int) = _uiState.value.month
-    fun setDay(dd: Int) = _uiState.value.day
 
 
 
