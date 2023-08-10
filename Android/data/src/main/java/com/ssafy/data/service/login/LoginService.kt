1bfb3e42f5346d27b4752d0b8b175aedbf9e0b22
JINWOO CHOI <csw3055@gmail.com>
2023-08-10 17:10:55 +0900

    [A_Feat] 로그인 시 유저id sharedpreference에 저장
    Ticket No: #S09P12A501-111

--- Android/data/src/main/java/com/ssafy/data/service/login/LoginService.kt
+++ Android/data/src/main/java/com/ssafy/data/service/login/LoginService.kt
@@ -1,14 +1,21 @@
 package com.ssafy.data.service.login
 
 import com.skydoves.sandwich.ApiResponse
-import com.ssafy.data.model.login.KakaoCheckRespDto
+import com.ssafy.data.model.login.req.JoinReqDto
+import com.ssafy.data.model.login.resp.CommRespDto
+import retrofit2.http.Body
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface LoginService {
     @GET("user/mobile/login")
     suspend fun kakaoLogin(
         @Query("token") token: String
-    ): ApiResponse<KakaoCheckRespDto>
+    ): ApiResponse<CommRespDto>
+
+    @POST("user/join")
+    suspend fun joinWithbirth(
+        @Body data: JoinReqDto
+    ): ApiResponse<CommRespDto>
 }
