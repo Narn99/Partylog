a39806f2f393e959817abe8ba52824d2c9af0dd8
JINWOO CHOI <csw3055@gmail.com>
2023-08-10 13:40:17 +0900

    [A_Feat] 응답 코드따라 화면전환 로직 추가
    Ticket No: #S09P12A501-111

--- Android/presentaion/src/main/java/com/ssafy/partylog/ui/login/LoginLayout.kt
+++ Android/presentaion/src/main/java/com/ssafy/partylog/ui/login/LoginLayout.kt
@@ -5,16 +5,11 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -30,26 +25,21 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
 import com.ssafy.partylog.R
-import com.ssafy.partylog.ui.login.stateholder.LoginFrameStateHolder
 import com.ssafy.partylog.ui.login.stateholder.LoginViewModel
 import com.ssafy.partylog.ui.theme.PartylogTheme
 import com.ssafy.partylog.ui.theme.loginTextColor
 import com.ssafy.partylog.ui.theme.maplestory
-import com.ssafy.partylog.util.ScreenState
-import com.ssafy.partylog.util.loginNav
+import com.ssafy.partylog.util.Util
 
 @Composable
 fun Login(
     modifier: Modifier = Modifier, font: FontFamily = maplestory,
     viewModel: LoginViewModel = hiltViewModel<LoginViewModel>(),
-    navi: NavHostController = loginNav()
+    navToMain: () -> Unit = {}, navToGetbirth: () -> Unit = {}
 ) {
 
     val loginCode = viewModel.loginCode
-
 
     Image(
         painter = painterResource(id = R.drawable.bg_login_compose),
@@ -74,7 +64,7 @@ fun Login(
                 .fillMaxWidth(), verticalArrangement = Arrangement.Bottom,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            LoginFrame(modifier, viewModel, loginCode,navi)
+            LoginFrame(modifier, viewModel, loginCode, navToGetbirth, navToMain)
         }
 
     }
@@ -83,24 +73,23 @@ fun Login(
 
 @Composable
 fun LoginFrame(modifier: Modifier, viewModel: LoginViewModel, loginCode: Int,
-               navi: NavHostController) {
-    val stateHolder = LoginFrameStateHolder(viewModel)
+               navToGetbirth: () -> Unit, navToMain: () -> Unit) {
     val context = LocalContext.current
     when(loginCode) {
-        200 -> navi.navigate(ScreenState.Main.name)
-        201 -> navi.navigate(ScreenState.Getbirth.name)
+        200 -> navToMain()
+        201 -> navToGetbirth()
         0 -> {}
         else -> {
             Toast.makeText(LocalContext.current, "에러", Toast.LENGTH_SHORT).show()
         }
     }
-
+    viewModel.resetCode()
     Image(imageVector = ImageVector.vectorResource(id = R.drawable.ic_login_kakaologin),
         contentDescription = "kakao",
         modifier = modifier
             .padding(bottom = 124.dp)
             .clickable {
-                stateHolder.onKakaoSelected(context)
+                Util(viewModel = viewModel).kakaoLogin(context)
             })
     }
 
