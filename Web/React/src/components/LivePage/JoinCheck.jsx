e9a430fa783b4420d9177e6f22558b2990c8c05e
Narn99 <vkdldjtmflr@naver.com>
2023-08-11 17:57:10 +0900

    [W-Feat] 라이브 페이지 진입 확인 추가

--- Web/React/src/components/LivePage/JoinCheck.jsx
+++ Web/React/src/components/LivePage/JoinCheck.jsx
@@ -1,11 +1,85 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button, Grid } from "@mui/material";
+// import { useNavigate } from "react-router-dom";
 
 function JoinCheck(props) {
+  const { setIsJoinCheck } = props;
+
+  useEffect(() => {
+    const bodyToDark = document.querySelector("body");
+    bodyToDark.classList.add("live-page");
+
+    return () => {
+      bodyToDark.classList.remove("live-page");
+    };
+  }, []);
+
+  // const navigate = useNavigate();
+
+  const handleJoinButton = () => {
+    setIsJoinCheck(true);
+  };
+
+  const handleExitButton = () => {
+    window.close();
+  };
+
   return (
-    <div style={{ height: "100vh", width: "100vw", backgroundColor: "black" }}>
-      <Grid container justifyContent={"center"} alignItems={"center"}></Grid>
-    </div>
+    <Grid
+      container
+      justifyContent={"space-evenly"}
+      alignItems={"center"}
+      style={{ height: "100vh" }}
+    >
+      <Grid
+        item
+        container
+        xs={12}
+        md={3}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <Button
+          variant="contained"
+          style={{
+            fontFamily: "MaplestoryOTFBold",
+            fontSize: "40px",
+            color: "white",
+            width: "100%",
+            height: "150px",
+            borderRadius: "40px",
+            texShadow: "0.1px 0.1px 4px #e892a4",
+          }}
+          onClick={handleJoinButton}
+        >
+          참가
+        </Button>
+      </Grid>
+      <Grid
+        item
+        container
+        xs={12}
+        md={3}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <Button
+          variant="contained"
+          style={{
+            fontFamily: "MaplestoryOTFBold",
+            fontSize: "40px",
+            color: "white",
+            width: "100%",
+            height: "150px",
+            borderRadius: "40px",
+            texShadow: "0.1px 0.1px 4px #e892a4",
+          }}
+          onClick={handleExitButton}
+        >
+          나가기
+        </Button>
+      </Grid>
+    </Grid>
   );
 }
 
--- Web/React/src/pages/BirthdayInput.jsx
+++ Web/React/src/pages/BirthdayInput.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import Cake from "../image/Cake.png";
+import Cake from "../assets/Cake.png";
 import "../css/BirthdayInput.css";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
