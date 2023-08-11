f4aca7e46e15de5e5010fdb9147736bdd3e4774c
Narn99 <vkdldjtmflr@naver.com>
2023-08-11 15:01:37 +0900

    [W-FEAT] 입장 체크용 버튼 생성

--- Web/React/src/components/LivePage/JoinCheck.jsx
+++ Web/React/src/components/LivePage/JoinCheck.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import { Button, Grid } from "@mui/material";
 
 function JoinCheck(props) {
-  return <div></div>;
+  return (
+    <div style={{ height: "100vh", width: "100vw", backgroundColor: "black" }}>
+      <Grid container justifyContent={"center"} alignItems={"center"}></Grid>
+    </div>
+  );
 }
 
 export default JoinCheck;
