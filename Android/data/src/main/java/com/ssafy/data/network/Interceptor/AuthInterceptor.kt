bd27bfb5c0ef954ef672a26a052444972935a980
JINWOO CHOI <csw3055@gmail.com>
2023-08-14 12:44:47 +0900

    [A_Fix] 인증로직 두번 돌아가는 문제 수정
    Ticket No: #S09P12A501-78

--- Android/data/src/main/java/com/ssafy/data/network/Interceptor/AuthInterceptor.kt
+++ Android/data/src/main/java/com/ssafy/data/network/Interceptor/AuthInterceptor.kt
@@ -1,30 +1,30 @@
 package com.ssafy.data.network.Interceptor
 
-import android.content.SharedPreferences
+import com.orhanobut.logger.Logger
 import com.ssafy.data.datasource.local.SharedPreference
-import com.ssafy.data.repository.LoginRepositoryImpl
-import com.ssafy.domain.repository.LoginRepository
-import com.ssafy.domain.usecase.login.GetidUsecase
-import dagger.hilt.android.qualifiers.ApplicationContext
 import okhttp3.Interceptor
 import okhttp3.Response
-import javax.inject.Inject
 
 class AuthInterceptor(private val sharedPreference: SharedPreference) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val origin = chain.request()
-        if (origin.url.encodedPath.equals("user/mobile/login", true)||
+
+        val req = if (origin.url.encodedPath.equals("user/mobile/login", true)||
             origin.url.encodedPath.equals("user/join", true)) {
             //jwt 필요없는 요청들
+            Logger.d("jwi없음")
+            origin.newBuilder().build()
         }
         else {
-            chain.proceed(origin.newBuilder().apply {
+            Logger.d("jwt추가")
+            origin.newBuilder().apply {
                 addHeader(
                     "Authorization",
                     "Bearer " + sharedPreference.getAccessToken()
                 )
-            }.build())
+            }.build()
         }
-      return chain.proceed(chain.request())
+
+      return chain.proceed(req)
     }
 }
