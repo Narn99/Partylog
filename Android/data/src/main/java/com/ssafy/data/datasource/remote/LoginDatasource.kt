d6258882e56e910b907bedf89e0cefebc93415f6
JINWOO CHOI <csw3055@gmail.com>
2023-08-14 12:45:17 +0900

    [A_Style] 함수줄정리
    Ticket No: #S09P12A501-78

--- Android/data/src/main/java/com/ssafy/data/datasource/remote/LoginDatasource.kt
+++ Android/data/src/main/java/com/ssafy/data/datasource/remote/LoginDatasource.kt
@@ -1,6 +1,5 @@
 package com.ssafy.data.datasource.remote
 
-import com.orhanobut.logger.Logger
 import com.skydoves.sandwich.ApiResponse
 import com.ssafy.data.model.login.req.JoinReqDto
 import com.ssafy.data.model.login.resp.CommRespDto
@@ -10,9 +9,6 @@ import javax.inject.Inject
 class LoginDatasource @Inject constructor(private val service: LoginService) {
     suspend fun checkKakaoToken(token: String): ApiResponse<CommRespDto> = service.kakaoLogin(token)
     suspend fun getJoin(data: JoinReqDto): ApiResponse<CommRespDto> = service.joinWithbirth(data)
-    suspend fun checkAccessToken(): ApiResponse<CommRespDto> {
-        Logger.d("왜 두번?")
-        return service.checkAccessToken()
-    }
-    suspend fun checkRefreshToken(str: String): ApiResponse<CommRespDto> = service.checkRefreshToken()
+    suspend fun checkAccessToken(): ApiResponse<CommRespDto> = service.checkAccessToken()
+    suspend fun checkRefreshToken(): ApiResponse<CommRespDto> = service.checkRefreshToken()
 }
